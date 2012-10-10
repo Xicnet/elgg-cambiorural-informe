9d084be1bab4659a5fa2b1383ffa894c30b04ae8
XICnet <code@xicnet.com>
2012-10-10 17:07:47 -0300

    Fix river display

--- languages/es.php
+++ languages/es.php
@@ -5,7 +5,7 @@ $Spanish = Array(
 				 'informe:group_pa' => 'Promotor Acesor', 
 				 'informe:error:missing:meeting_manager' => 'Falta el nombre del representante',
 				 'informe:error:missing:building' => 'Falta el establecimiento',
-	'river:create:object:informe' => 'Informe mensual publicado',
+	'river:create:object:informe' => '%s ha publicado el %s',
 	'informe:empty:field' => '-',
 );
 
