8729899a502d75dd6fa99e2d7d5ed4113174155f
XICnet <code@xicnet.com>
2012-10-10 21:24:35 -0300

    Minor typo.

--- languages/es.php
+++ languages/es.php
@@ -2,7 +2,7 @@
 
 $Spanish = Array(
 				 'informe:group_ap' => 'Agente de Proyecto',
-				 'informe:group_pa' => 'Promotor Acesor', 
+				 'informe:group_pa' => 'Promotor Asesor', 
 				 'informe:error:missing:meeting_manager' => 'Falta el nombre del representante',
 				 'informe:error:missing:building' => 'Falta el establecimiento',
 	'river:create:object:informe' => '%s ha publicado el %s',
