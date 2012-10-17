22b68a1d9d5d610a9d3c42bb7c89885588ca5732
XICnet <code@xicnet.com>
2012-10-17 17:23:59 -0300

    Add sanity check

--- languages/es.php
+++ languages/es.php
@@ -8,6 +8,7 @@ $Spanish = Array(
 				 'informe:group' => 'Informes mensuales',
 				 'informe:error:missing:meeting_manager' => 'Falta el nombre del representante',
 				 'informe:error:missing:building' => 'Falta el establecimiento',
+				'informe:error:nocontainer' => 'El informe debe pertenecer a un grupo',
 	'river:create:object:informe' => '%s ha publicado el %s',
 	'informe:empty:field' => '-',
 	'river:create:object:default' => '%s creó %s',
