012052da621c3c6a64c4d83c33f95c1677e5e715
XICnet <code@xicnet.com>
2013-08-07 11:36:39 -0300

    Fix permissions for informe edit to allow admin and AP

--- classes/ElggInforme.php
+++ classes/ElggInforme.php
@@ -57,7 +57,7 @@ class ElggInforme extends ElggObject {
 		$user_guid = $user->guid;
 		$group = $this->getContainerEntity();
 
-		return (elgg_get_ignore_access() || (parent::canEdit() && ($user_guid == $group->pa || $this->meeting_pa == $user_guid)));
+		return (elgg_get_ignore_access() || (parent::canEdit() && ($user->isAdmin() == 1 || $user_guid == $group->pa || $this->meeting_pa == $user_guid || $this->meeting_ap == $user_guid)));
 	}
 
 	public function getSummary() {
